cb5f0dda2f0192bb2fc1e926ae0965558a5ce4dc
DevXo <85026428+mihka445@users.noreply.github.com>
2022-02-06 17:29:23 +0000

    Encode movie01

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("disstrack2","disstrack2.mp4",.15,.4,10)
+    encoder.encode("movie01","movie01.mp4",.15,.4,10)
 
 if __name__== "__main__":
     main()
